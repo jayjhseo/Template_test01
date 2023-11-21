288d4adfa4ba1a5f1c720cd21db7b4ad0b59daaf
jayjhseo <jayjhseo@gmail.com>
2023-11-21 12:22:27 +0900

    setting

---
+++ src/test/java/com/test01/sbb4/Sbb4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test01.sbb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
